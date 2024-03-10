ff955d4ff72bafb78b3b681dfad2c50cf039e6ff
Hygor Henrique <101516308+Hygorhhss@users.noreply.github.com>
2024-03-10 16:09:00 -0300

    atualização

--- meuProjeto/app.py
+++ meuProjeto/app.py
@@ -1,32 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import random
 
 app = Flask(__name__)
 
-def escolher_filme(lista_filmes):
-    """Função para escolher um filme aleatório da lista."""
-    filme_escolhido = random.choice(lista_filmes)
-    return filme_escolhido
-
-# Lista de filmes
-lista_de_filmes = [
-    "O Poderoso Chefão",
-    "Matrix",
-    "Titanic",
-    "Cidade de Deus",
-    "Pulp Fiction",
-    "Interestelar",
-    "O Senhor dos Anéis: A Sociedade do Anel",
-    "Vingadores: Ultimato",
-    "A Origem",
-    "Forrest Gump"
-]
-
-# Escolhendo um filme aleatório
-filme_aleatorio = escolher_filme(lista_de_filmes)
+# Lista de todos os filmes e suas categorias
+filmes = {
+    "O Poderoso Chefão": "acao",
+    "Matrix": "acao",
+    "Titanic": "romance",
+    "Cidade de Deus": "acao",
+    "Pulp Fiction": "acao",
+    "Interestelar": "ficcao",
+    "O Senhor dos Anéis: A Sociedade do Anel": "fantasia",
+    "Vingadores: Ultimato": "acao",
+    "A Origem": "ficcao",
+    "Forrest Gump": "drama"
+}
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/questionario')
+def questionario():
+    return render_template('questionario.html')
+
+@app.route('/escolher_filme', methods=['POST'])
+def escolher_filme():
+    categoria = request.form['categoria']
+    if categoria == 'todos':
+        filmes_disponiveis = list(filmes.keys())
+    else:
+        filmes_disponiveis = [filme for filme, cat in filmes.items() if cat == categoria]
+
+    filme_aleatorio = random.choice(filmes_disponiveis)
     return render_template('index.html', filme_aleatorio=filme_aleatorio)
 
 if __name__ == '__main__':
